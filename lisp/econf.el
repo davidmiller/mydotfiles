5ee2a28b9635f5ff5ceea299118c38dbb6a79e02
David Miller <david@rasputin.(none)>
2011-02-20 14:54:16 +0000

    Adding django mode and some conf updates

--- lisp/econf.el
+++ lisp/econf.el
@@ -128,3 +128,7 @@
 ;; So I'll be lowercasin'
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+;; Shell
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
